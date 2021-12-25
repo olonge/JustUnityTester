74d46ac2e72f3458c9618430d85bffc1abf3c8be
YOMI-NUC\Yomi <yomilonge@hotmail.com>
2021-12-25 23:04:23 +0000

    "GetAllLoadedScenesCommand return scene by build index and not by loaded scenes"

--- AltUnityServer/Commands/UnityCommands/AltUnityGetAllLoadedScenesCommand.cs
+++ AltUnityServer/Commands/UnityCommands/AltUnityGetAllLoadedScenesCommand.cs
@@ -8,7 +8,8 @@
         public override string Execute() {
             AltUnityRunner._altUnityRunner.LogMessage("getAllLoadedScenes");
             for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++) {
-                var sceneName = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+
+                var sceneName = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i).name;
                 sceneNames.Add(sceneName);
             }
             return Newtonsoft.Json.JsonConvert.SerializeObject(sceneNames);
