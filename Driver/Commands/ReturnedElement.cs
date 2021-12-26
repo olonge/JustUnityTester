758ecc2d214cc3904535365bfeb666ae5db30db8
YOMI-NUC\Yomi <yomilonge@hotmail.com>
2021-12-26 19:06:28 +0000

    Makes ReturnedElement clearer

--- Driver/Commands/ReturnedElement.cs
+++ Driver/Commands/ReturnedElement.cs
@@ -1,32 +1,35 @@
+using System.Collections.Generic;
 using JustUnityTester.Core;
+using Newtonsoft.Json;
 
 namespace JustUnityTester.Driver.Commands {
     public class ReturnedElement : BaseCommand {
-        public ReturnedElement(SocketSettings socketSettings) : base(socketSettings) {
-        }
+        public ReturnedElement(SocketSettings socketSettings) : base(socketSettings) { }
 
         protected TestObject ReceiveAltUnityObject() {
             string data = Recvall();
+
             if (!data.Contains("error:")) {
-                TestObject altElement = Newtonsoft.Json.JsonConvert.DeserializeObject<TestObject>(data);
-                altElement.socketSettings = SocketSettings;
-                return altElement;
+                var testElement = JsonConvert.DeserializeObject<TestObject>(data);
+                testElement.socketSettings = SocketSettings;
+                return testElement;
             }
-            HandleErrors(data);
-            return null;
-        }
-        protected System.Collections.Generic.List<TestObject> ReceiveListOfAltUnityObjects() {
-            string data = Recvall();
-            if (!data.Contains("error:")) {
-                var altElements = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Collections.Generic.List<TestObject>>(data);
-                foreach (var altElement in altElements) {
-                    altElement.socketSettings = SocketSettings;
-                }
-                return altElements;
-            }
+
             HandleErrors(data);
             return null;
         }
 
+        protected List<TestObject> ReceiveListOfAltUnityObjects() {
+            string data = Recvall();
+            if (!data.Contains("error:")) {
+                var altElements = JsonConvert.DeserializeObject<List<TestObject>>(data);
+                foreach (var altElement in altElements)
+                    altElement.socketSettings = SocketSettings;
+                return altElements;
+            }
+
+            HandleErrors(data);
+            return null;
+        }
     }
 }
