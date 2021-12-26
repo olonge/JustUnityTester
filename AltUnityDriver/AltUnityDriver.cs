fc263a09b240d963f1f0e85ce5b74321905c20e5
YOMI-NUC\Yomi <yomilonge@hotmail.com>
2021-12-26 04:52:37 +0000

    Moves SocketSettings into own file

--- AltUnityDriver/AltUnityDriver.cs
+++ AltUnityDriver/AltUnityDriver.cs
@@ -2,21 +2,7 @@ using Assets.AltUnityTester.AltUnityDriver.Commands.InputActions;
 using Assets.AltUnityTester.AltUnityDriver.UnityStruct;
 
 public enum PLayerPrefKeyType { Int = 1, String, Float }
-public struct SocketSettings
-{
-    public System.Net.Sockets.TcpClient socket;
-    public string requestSeparator;
-    public string requestEnding;
-    public bool logFlag;
 
-    public SocketSettings(System.Net.Sockets.TcpClient socket, string requestSeparator, string requestEnding,bool logFlag)
-    {
-        this.socket = socket;
-        this.requestSeparator = requestSeparator;
-        this.requestEnding = requestEnding;
-        this.logFlag = logFlag;
-    }
-}
 public class AltUnityDriver
 {
     public System.Net.Sockets.TcpClient Socket;
---
+++ AltUnityDriver/SocketSettings.cs
@@ -0,0 +1,13 @@
+﻿public struct SocketSettings {
+    public System.Net.Sockets.TcpClient socket;
+    public string requestSeparator;
+    public string requestEnding;
+    public bool logFlag;
+
+    public SocketSettings(System.Net.Sockets.TcpClient socket, string requestSeparator, string requestEnding, bool logFlag) {
+        this.socket = socket;
+        this.requestSeparator = requestSeparator;
+        this.requestEnding = requestEnding;
+        this.logFlag = logFlag;
+    }
+}
